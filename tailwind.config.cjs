f5fac22108e1e4434a3c1f2d57700c2a5d4094da
silva <ericoderito@gmail.com>
2022-07-17 15:50:21 +0300

    set up project structure
    create project using vite
    add tailwind support to vite
    add static assets to project
    extend tailwind theme by adding projet requirements

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "p-orange": "hsl(25, 97%, 53%)",
+        "n-white": "hsl(0, 0%, 100%)",
+        "n-light-gray": "hsl(217, 12%, 63%)",
+        "n-medium-gray": "hsl(216, 12%, 54%)",
+        "n-dark-blue": " hsl(213, 19%, 18%)",
+        "n-very-dark-blue": "hsl(216, 12%, 8%)",
+      },
+      fontFamily: {
+        body: ["Overpass", "san-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
