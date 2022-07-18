178737d1ad6ae027e8c77c63a321e3117388865e
silva <ericoderito@gmail.com>
2022-07-18 09:48:48 +0300

    add css
    this should include states such as active

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,9 +7,9 @@ module.exports = {
         "p-orange": "hsl(25, 97%, 53%)",
         "n-white": "hsl(0, 0%, 100%)",
         "n-light-gray": "hsl(217, 12%, 63%)",
-        "n-medium-gray": "hsl(216, 12%, 54%)",
-        "n-dark-blue": " hsl(213, 19%, 18%)",
-        "n-very-dark-blue": "hsl(216, 12%, 8%)",
+        "n-medium-gray": "hsl(210, 19%, 18%)",
+        "n-dark-blue": "hsl(215, 20%, 16%)",
+        "n-very-dark-blue": "hsl(228, 11%, 9%)",
       },
       fontFamily: {
         body: ["Overpass", "sans-serif"],
